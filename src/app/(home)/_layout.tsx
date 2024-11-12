0f7cbec45507dd834a19719f318ed586bc414cdd
Ogoigbe1 <ogoigbe12@gmail.com>
2024-11-12 08:31:02 +0100

    setup

---
+++ src/app/(home)/_layout.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Slot, Stack } from 'expo-router'
+
+export default function HomeLayout() {
+  return (
+    // <View>
+    //   <Text>HomeLayout</Text>
+    // </View>
+    <Slot/>
+  )
+}
---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+import { Redirect } from 'expo-router'
+
+export default function HomeScreen() {
+  return (
+    <Redirect href={'/(home)/(tabs)'}/>
+  )
+}
+
+
